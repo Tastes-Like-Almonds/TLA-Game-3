902187cc8e878b018595f6d7d871fa575d2a7e07
Aidan Wignall <aidanwignall2009@gmail.com>
2025-10-15 17:45:05 -0700

    Basic multiplayer lobby (from godot docs)

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -561,7 +561,7 @@ func _ready() -> void:
 	lives = get_modified_property("max_lives")
 	add_weapon(get_modified_property("starting_weapon"))
 	equip_weapon_slot(0)
-	Input.mouse_mode = Input.MOUSE_MODE_CONFINED # TODO Move to a better spot when level loading is better
+	#Input.mouse_mode = Input.MOUSE_MODE_CONFINED # TODO Move to a better spot when level loading is better
 
 ## Set the last kinematic collision of the sword tip. Should be done each physics process.
 func set_last_collision(collision:KinematicCollision2D) -> void:
