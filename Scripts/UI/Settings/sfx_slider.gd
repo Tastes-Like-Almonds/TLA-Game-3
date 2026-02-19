f78de060706e90115790d99fd753f1ce5e2ca0eb
Aidan Wignall <aidanwignall2009@gmail.com>
2026-02-19 13:20:14 -0800

    Brightness setting

--- Scripts/UI/Settings/sfx_slider.gd
+++ Scripts/UI/Settings/sfx_slider.gd
@@ -1,21 +1,16 @@
 extends HSlider
 
-func changed() -> void:
-	var idx := AudioServer.get_bus_index("SFX")
-	AudioServer.set_bus_volume_linear(idx, value)
-
 func _ready() -> void:
 	PersistentData.DataLoaded.connect(
 		func(key:String, val:Variant) -> void:
-			if key == "sfx_volume_perc":
+			if key == "brightness":
 				value = val
-				changed()
 	)
 	drag_ended.connect(
 		func(vc:bool) -> void:
 			if vc:
-				GameSettings.set_setting("sfx_volume_perc", value)
-				changed()
+				GameSettings.set_setting("brightness", value)
 	)
-	value = GameSettings.get_setting("sfx_volume_perc")
-	changed()
+	var brightness : Variant = GameSettings.get_setting("brightness")
+	if brightness:
+		value = brightness
